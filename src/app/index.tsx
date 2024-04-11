57c555a416c46f27364aeb647009ca73f1b7d969
Ramit <ramitsingh12@gmail.com>
2024-04-11 12:34:55 +0530

    stepzen api

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,40 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList, TextInput, Button } from "react-native";
 import FoodListItem from "../components/FoodListItem";
+import { useState } from "react";
+
+const foodItems = [
+  { label: "Pizza", cal: 475, brand: "domoinos" },
+  { label: "Apple", cal: 75, brand: "Generic" },
+  { label: "Black Coffee", cal: 10, brand: "Americano" },
+];
 
 export default function App() {
+  const [search, setSearch] = useState("");
+
+  const performSearch = () => {
+    console.warn ('Searching for:', search);
+
+    setSearch('');
+  }
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <FoodListItem item={{label:"Pizza",cal:475, brand:"domoinos"}}/>
-      <FoodListItem item={{label:"Apple",cal:75, brand:"Generic"}}/>
+
+      <TextInput
+        value={search}
+        onChangeText={setSearch}
+        placeholder="Search.."
+        style={styles.input}
+      />
+      { search &&  <Button color={'#000000'} title="Search" onPress={performSearch}/> }
+      <FlatList
+        scrollEnabled
+        data={foodItems}
+        renderItem={({ item }) => <FoodListItem item={item} />}
+        contentContainerStyle={{ gap: 5 }}
+      />
     </View>
   );
 }
@@ -16,8 +43,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#252525",
-    justifyContent: "center",
     padding: 10,
-    gap:5,
+    gap: 10,
+  },
+  input: {
+    backgroundColor: "#5A5A5A",
+    borderRadius: 5,
+    padding: 10,
+    color: "#fff",
   },
 });
--- src/components/FoodListItem.tsx
+++ src/components/FoodListItem.tsx
@@ -1,28 +1,31 @@
-import { Text, View } from "react-native"
+import { Text, View, StyleSheet } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 
-const FoodListItem = ({item}) => {
-    return (
-      <View
-      style={{
-        backgroundColor: "#5A5A5A",
-        padding: 10,
-        borderRadius: 5,
-        flexDirection:'row',
-        alignItems:'center',
-        justifyContent:'space-between',
-      }}
-    >
-      <View style={{flex:1, gap: 5,}}>
+const FoodListItem = ({ item }) => {
+  return (
+    <View style={styles.container}>
+      <View style={{ flex: 1, gap: 5 }}>
         <Text style={{ fontWeight: "bold", fontSize: 16, color: "#fff" }}>
           {item.label}
         </Text>
-        <Text style={{ color: "#ccc" }}>{item.cal} cal, {item.brand}</Text>
+        <Text style={{ color: "#ccc" }}>
+          {item.cal} cal, {item.brand}
+        </Text>
       </View>
-  
       <AntDesign name="pluscircleo" size={24} color="#191919" />
     </View>
-    )
-  }
-  
-  export default FoodListItem;
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#5A5A5A",
+    padding: 10,
+    borderRadius: 5,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+});
+
+export default FoodListItem;
